67a58cf8a44f6e2d5003cc9167e252f108a250b7
Aida <aidar.ospan@list.ru>
2023-02-13 13:30:08 +0600

    first commit

---
+++ Authentication/Controllers/AuthViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Authentication
+//
+//  Created by Айдар Оспанов on 13.02.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    
+    @IBOutlet var loginTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let yourPage = segue.destination as? YourPageViewController else {
+            return
+        }
+        guard let loginText = loginTF.text, !loginText.isEmpty else { return }
+        yourPage.nameLogin = "Welcome, \(loginText)"
+    }
+    
+
+    @IBAction func loginButtonTapped() {
+        guard let loginText = loginTF.text, let passwordText = passwordTF.text,
+              !loginText.isEmpty, !passwordText.isEmpty else {
+            showAlert(
+                title: "Login or password is empty",
+                message: "Please enter your login or password")
+            return
+        }
+        
+        guard passwordText == "123", loginText == "Jinx" else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please enter correct login and password"
+            )
+            return
+        }
+        
+    }
+    
+    @IBAction func userButtonTapped() {
+        showAlert(title: nil, message: "your login is Jinx 🙂")
+    }
+    
+    @IBAction func passwordButtonTapped() {
+        showAlert(title: nil, message: "your password is 123 🙂")
+    }
+}
+
+extension AuthViewController {
+    
+    func showAlert(title: String?, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: .default)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+}
---
+++ Authentication/Controllers/YourPageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  YourPageViewController.swift
+//  Authentication
+//
+//  Created by Айдар Оспанов on 13.02.2023.
+//
+
+import UIKit
+
+class YourPageViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    var nameLogin: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = nameLogin
+    }
+    
+    @IBAction func logOutButtonTapped() {
+        dismiss(animated: true)
+    }
+}
