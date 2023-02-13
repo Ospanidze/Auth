b233545b7ede9641d7ed95fba18606179478bb56
Aida <aidar.ospan@list.ru>
2023-02-13 15:13:27 +0600

    NSNotifaction

--- Authentication/Controllers/AuthViewController.swift
+++ Authentication/Controllers/AuthViewController.swift
@@ -9,9 +9,55 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    @IBOutlet var mainStackView: UIStackView!
     
     @IBOutlet var loginTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(notifaction:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(notifaction: NSNotification) {
+        guard let keyboardFrame = notifaction.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        
+        guard keyboardHeight > mainStackView.frame.origin.y else  { return }
+        let bottomSpace = self.view.frame.height - (mainStackView.frame.origin.y + mainStackView.frame.height)
+        self.view.frame.origin.y -= keyboardHeight - bottomSpace + 20
+    }
+    
+    @objc private func keyboardWillHide() {
+        self.view.frame.origin.y = 0
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let yourPage = segue.destination as? YourPageViewController else {
@@ -53,6 +99,12 @@ final class AuthViewController: UIViewController {
     @IBAction func passwordButtonTapped() {
         showAlert(title: nil, message: "your password is 123 🙂")
     }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        
+        loginTF.text = ""
+        passwordTF.text = ""
+    }
 }
 
 extension AuthViewController {
@@ -60,7 +112,9 @@ extension AuthViewController {
     func showAlert(title: String?, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let okAction = UIAlertAction(title: "ok", style: .default)
+        let okAction = UIAlertAction(title: "ok", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
         alert.addAction(okAction)
         
         present(alert, animated: true)
